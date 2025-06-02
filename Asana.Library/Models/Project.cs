09b1e04eab4d1e8958b8b25521e216854a660e1a
HollowScythe3791 <silaswthorn@gmail.com>
2025-06-01 17:21:33 -0700

    Synce changes

--- Asana.Library/Models/Project.cs
+++ Asana.Library/Models/Project.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Asana.Library.Models
 {
-    public class ToDo
+    public class Project
     {
         public int Id { get; set; }
         public string? Name { get; set; }
